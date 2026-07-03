45c4002495887b293b04df171c7c92b7d990642f
Oluwaseyi David <oluwaseyidavid26@gmail.com>
2026-07-03 12:16:29 -0700

    use-state

--- Navigation/Navbar.tsx
+++ Navigation/Navbar.tsx
@@ -64,7 +64,7 @@ export default function Navbar() {
     <nav className="fixed top-0 left-0 w-full z-50 bg-[#1A1A1A] border-b border-white/10">
       <div className="max-w-7xl mx-auto px-6 flex items-center justify-between h-16">
         {/* Logo */}
-        <Link href="/" className="flex items-center gap-2">
+        <Link href="/" className="flex items-center gap-2 ml-4">
           <Image
             src="/icon-name-white.png"
             alt="Hand Held Media Group"
